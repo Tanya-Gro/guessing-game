120b5892168e8cc748b03629b2e165c67a863fb8
Tanya-Gro <tarpel.gta@gmail.com>
2024-10-27 20:30:05 +0200

    add: answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.candidate = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        // let candidate = (Math.round((this.max - this.min) / 2) + this.min);
+        this.candidate = Math.round((this.max + this.min) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
